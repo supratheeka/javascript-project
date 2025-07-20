d3b663b1f3debd4015afe1a9dcc857176c765874
supratheeka <supratheekayadav@gmail.com>
2025-07-20 14:00:00 +0530

    javascript functions

---
+++ function.js
@@ -0,0 +1,14 @@
+function apple() {
+  console.log(`I like apples`);
+}
+apple();
+// expression function
+const mango= function() {
+  console.log(`I like mangoes`);
+}
+mango();
+// arrow function
+const banana = () => {
+  console.log(`I like bananas`);
+}
+banana();
---
+++ template.js
@@ -0,0 +1,2 @@
+var Example="Hello Javascript";
+console.log(` Welcome to jvasript World{Example}\nThis is my first program`)
