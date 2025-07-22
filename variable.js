91fe8215873d189b1bbd7c434f182eb58a4c95e9
supratheeka <supratheekayadav@gmail.com>
2025-07-22 22:09:04 +0530

    variables in javascript

---
+++ variable.js
@@ -0,0 +1,31 @@
+//var variable
+function testvar() {
+    var x = 10;
+    if (true) {
+        var x = 20; // same variable
+        console.log(x); // 20
+    }
+    console.log(x); // 20
+}
+
+//let variable
+function testlet() {
+    let y = 10;
+    if (true) {
+        let y = 20; // different variable
+        console.log(y); // 20
+    }
+    console.log(y); // 10
+}
+testlet();
+
+//const variable
+function testconst() {
+    const z = 10;
+    if (true) {
+        // const z = 20; // This would cause an error
+        console.log(z); // 10
+    }
+    console.log(z); // 10
+}
+testconst();
